f60e5be701092ed8940f9bb264b86eefc90cc1ff
Luis Suchil <jlsuchil@gmail.com>
2022-11-13 09:59:39 -0600

    Primer comit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Preloader
+$(window).on('load', function() {
+	$('#preloader').delay(100).fadeOut('slow',function(){$(this).remove();});
+  });
+
+// Menu
+$(function () {
+$('.dropdown-toggle').on('click', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  
+    var self = $(this);
+    if(self.is('.disabled, :disabled')) {
+      return false;
+    }
+    self.parent().toggleClass("open");
+  });
+  
+  $(document).on('click', function(e) {
+    if($('.dropdown').hasClass('open')) {
+      $('.dropdown').removeClass('open');
+    }
+  });
+  
+  $('.nav-btn.nav-slider').on('click', function() {
+    $('.overlay').show();
+    $('nav').toggleClass("open");
+  });
+  
+  $('.overlay').on('click', function() {
+    if($('nav').hasClass('open')) {
+      $('nav').removeClass('open');
+    }
+    $(this).hide();
+  })});
